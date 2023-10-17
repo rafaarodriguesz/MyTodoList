1f8fa196d1662e761fdffb9280d4f419bb7cea70
Rafael <rr65887@gmail.com>
2023-10-16 21:26:09 -0300

    Finalizando git hub pages

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Ooooi To criando o projeto, sempre q eu fizer um deploy vc consegue ver por aqui hehe bjsbjs do rafa</h1>
     </div>
   );
 }
